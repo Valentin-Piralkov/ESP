0df4bd54fe0f375afa6fbea8b1abcc34b40e1476
gabedonnan <47415809+gabedonnan@users.noreply.github.com>
2022-02-23 19:11:31 +0000

    Added biomes + accurate height maps
    Added accurate height maps (i.e. not just random colours on the screen, but proper height gradients), alongside biome allocation along the map.
    
    -note: runtimes getting rather large so must consider multithreading this process from now

--- MapGen.js
+++ MapGen.js
@@ -38,8 +38,20 @@ class MapGenerator {
       138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180];
     this.size = size;
     this.parameters = parameters;
-    this.map = Array.from({ length: size }, () => (Array.from({ length: size }, () => (new MapNode(0, 0)))));
+    this.map = [];
 
+  }
+
+  loadMap(arr,bioArr, startOffset){
+    let length1 = arr.length;
+    let length2 = arr[0].length;
+    this.map = [];
+    for (let i = startOffset; i < length1; i++){
+      (this.map).push([]);
+      for (let j = startOffset; j < length2; j++){
+        (this.map[i-startOffset]).push(new MapNode(arr[i][j],bioArr[i][j]));
+      }
+    }
   }
 
   convolution(arr, kernel){
@@ -62,10 +74,13 @@ class MapGenerator {
 
   average(arr, size){
     let arrLen = arr.length;
-    let newMap = Array.from({ length: arrLen }, () => (Array.from({ length: arrLen }, () => (0))));
+    let newMap = [];
+    //let newMap = Array.from({ length: arrLen }, () => (Array.from({ length: arrLen }, () => (0))));
     let counter = 0;
     for (let i = 0; i < arrLen; i++){
+      newMap.push([]);
       for (let j = 0; j < arrLen; j++){
+        newMap[i].push(0);
         counter = 0;
         for (let k = -size; k < size+1; k++){
           for (let l = -size; l < size+1; l++){
@@ -81,7 +96,7 @@ class MapGenerator {
     return newMap;
   }
 
-  level(arr, level, rounding){
+  level(arr, rounding){
     let arrLen = arr.length;
     for (let i = 0; i < arrLen; i++){
       for (let j = 0; j < arrLen; j++){
@@ -107,12 +122,21 @@ class MapGenerator {
     return mapArray;
   }
   
-  arrAdd2D(arr1, arr2, avg){
+  arrAdd2D(arr1, arr2, avg, mirrorI, mirrorJ){
     let length1 = arr1.length;
     let length2 = arr1[0].length;
     for (let i = 0; i < length1; i++){
       for (let j = 0; j < length2; j++){
-        arr1[i][j] += arr2[i][j];
+        arr1[i][j] += arr2[(!mirrorI) ? i : length1-i-1][(!mirrorJ) ? j : length2-j-1]; //Slows down the code like a motherfucker but it looks nice so im keeping it for now
+        /*if (mirrorI && mirrorJ){
+          arr1[i][j] += arr2[length1-i-1][length2-j-1];
+        } else if (mirrorI) {
+          arr1[i][j] += arr2[length1-i-1][j];
+        } else if (mirrorJ) {
+          arr1[i][j] += arr2[i][length2-j-1];
+        } else {
+          arr1[i][j] += arr2[i][j];
+        }*/
         if (avg) {
           arr1[i][j] /= 2;
         }
@@ -185,6 +209,7 @@ class MapGenerator {
     return 45.23065*(n0 + n1 + n2);
   }
 }
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
@@ -192,25 +217,39 @@ var ctx = canvas.getContext("2d");
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 mapGen = new MapGenerator(1200,[""]);
 
-let mapArray = mapGen.arrAdd2D(mapGen.layerNoise(Math.floor(Math.random()*25)+4),mapGen.layerNoise(Math.floor(Math.random()*5)+4), true); //Looks complicated but is actually pretty simple, adds two randomly selected layerings of noise together to form a more realistic and varied map
-
-let gaussArray7 = [[1,4,7,10,7,4,1],[4,12,26,33,26,12,4],[7,26,55,71,55,26,7],[10,33,71,91,71,33,10],[7,26,55,71,55,26,7],[4,12,26,33,26,12,4],[1,4,7,10,7,4,1]];
+console.time("Noise Layering");
+let mapArray = mapGen.arrAdd2D(mapGen.layerNoise(Math.floor(Math.random()*24)+7),mapGen.layerNoise(Math.floor(Math.random()*5)+7), true, true, false); //Looks complicated but is actually pretty simple, adds two randomly selected layerings of noise together to form a more realistic and varied map
+console.timeEnd("Noise Layering");
+/*let gaussArray7 = [[1,4,7,10,7,4,1],[4,12,26,33,26,12,4],[7,26,55,71,55,26,7],[10,33,71,91,71,33,10],[7,26,55,71,55,26,7],[4,12,26,33,26,12,4],[1,4,7,10,7,4,1]];
 for (let i=0; i < gaussArray7.length; i++){
   for (let j = 0; j < gaussArray7[0].length; j++){
     gaussArray7[i][j] *= 1/1100;
   }
-}
-let newMap = mapGen.convolution(mapArray, gaussArray7, 7);
-newMap = mapGen.average(newMap, 8);
+}*/
+//let newMap = mapGen.convolution(mapArray, gaussArray7, 7);
+console.time("Averaging");
+let newMap = mapGen.average(mapArray, 8);
+console.timeEnd("Averaging");
 
-newMap = mapGen.level(newMap, 0.001, 10000);
+let biomeMap = mapGen.level(mapGen.average(mapArray, 13), 1000);
 
-for (let i = -1, row = 0; i < 1; i+=0.00166666, row++){
-  for (let j = -1, col = 0; j < 1; j+=0.001666666, col++)
+console.time("leveling");
+newMap = mapGen.level(newMap, 10000);
+console.timeEnd("leveling");
+console.time("map loading");
+mapGen.loadMap(newMap, biomeMap, 13);
+console.timeEnd("map loading");
+for (let i = -0.9783333, row = 13; i < 1; i+=0.00166666, row++){
+  for (let j = -0.9783333, col = 13; j < 1; j+=0.001666666, col++)
   {
-    //(this.map[row][col]).setHeight(newMap[row][col]); 
-    ctx.fillStyle = "#00"+parseInt((newMap[row][col]*8388607)).toString(16);
+    
+    if (biomeMap[row][col] == 0.001){
+      ctx.fillStyle = "#00"+parseInt((newMap[row][col]*35000)).toString(16) + "00";
+    }
+    else { 
+      ctx.fillStyle = "#"+parseInt((newMap[row][col]*40000)).toString(16).repeat(3);
+    }
     ctx.fillRect((i+1)*600, (j+1)*600, 1, 1);
-    //console.log("#"+parseInt((newMap[row][col]*8388607)).toString(16));
+    //console.log(biomeMap[row][col]);
   }
 }
